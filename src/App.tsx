cc8bb9d52ea887c05cb23f9ccfc75e30340b778e
Atishay Jain <53376050+itsatishay@users.noreply.github.com>
2025-04-30 01:48:11 -0700

    fixed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header';
 import SearchBar from './components/SearchBar/SearchBar';
 import ExampleList from './components/ExampleList/ExampleList';
